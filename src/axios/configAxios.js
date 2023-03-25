b0e9d872b20f80e8ed3a6632c725cb13c4bff62b
anhcoder03 <nguyenphianh161003@gmail.com>
2023-03-25 23:57:40 +0700

    update

--- src/axios/configAxios.js
+++ src/axios/configAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://api-book1.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
