317d5d6e4154932a4e7de311e12a68b036b9b07a
anhcoder03 <nguyenphianh161003@gmail.com>
2023-04-03 18:23:51 +0700

    fix jwt

--- src/modules/dashboard/DashboardHeader.jsx
+++ src/modules/dashboard/DashboardHeader.jsx
@@ -37,7 +37,7 @@ const DashboardHeader = () => {
         <img
           src={
             user?.image
-              ? user.avatar
+              ? user.image
               : "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80"
           }
           alt=""
